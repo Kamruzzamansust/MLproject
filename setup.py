23a901eb64e6618af0654194334fef0ed4cddcd1
Kamruzzamansust <kamruzzzaman.sust15@gmail.com>
2024-01-23 08:12:18 -0800

    second commit-- project finished

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(filepath:str)->List[str]:
+    requirements = []
+
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+setup(
+  name = 'mlproject',
+  version = '0.0.1',
+  author = 'Kamruzzaman',
+  author_email = 'kamruzzaman.sust15@gmail.com',
+  packages = find_packages(),
+  install_requires = get_requirements('requirements.txt'),
+
+
+)
